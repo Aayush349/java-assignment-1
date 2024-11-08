bac2543826198da4a07ed30f7263b4ec22be78b7
Aayush349 <aayushsahu867@gmail.com>
2024-11-08 22:54:34 +0530

    check positive and negative

---
+++ Q2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Q2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x ;
+        x=sc.nextInt();
+
+if(x==0)
+        {System.out.println("no is 0 nor positive nither negative ");}
+
+
+        if (x>0)
+        {
+            System.out.println("posivite ");
+
+        }
+        else{
+            System.out.println("negative ");
+        }
+        
+    }
+}
